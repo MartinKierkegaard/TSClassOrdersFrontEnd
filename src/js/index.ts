e042046beb8dbcd3ed70cd6042a05a40238ad256
Martin Kierkegaard <mark@easj.dk>
2018-11-06 16:35:07 +0100

    tester OrdreLinie klasse

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,15 @@
-interface Person {
-    firstName: string;
-    lastName: string;
-}
+import {IOrdreLinie} from "./IOrdreLinie"
+import {OrdreLinie} from "./OrdreLinie";
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
+function greeter(ol: OrdreLinie): string {
+    return "<h3>" + ol.orderQty + " " + ol.productID + "</h3>";
 }
-let user: Person = { firstName: "John", lastName: "Doe" };
+// let user: Person = { firstName: "John", lastName: "Doe" };
+let ol1 : IOrdreLinie = {ProductID:"123", 
+                         OrderQty:3,
+                        UnitPrice:34,
+                        UnitPriceDiscount:1}
+let orderLinie : OrdreLinie = new OrdreLinie(ol1);
 
 let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+element.innerHTML = greeter(orderLinie);
